8af648d4189de0599a285259e7462c868ce82695
NMoua-Edu <73411759+NMoua-Edu@users.noreply.github.com>
2020-11-27 21:07:05 -0500

    Fix white space

--- src/main/java/edu/oakland/production/middleware02/MiddlewareCommInterfaceImplementation.java
+++ src/main/java/edu/oakland/production/middleware02/MiddlewareCommInterfaceImplementation.java
@@ -4,16 +4,13 @@ import edu.oakland.helper.admin.TrackData;
 import edu.oakland.production.middleware02.MiddlewareCommInterface;
 
 public class MiddlewareCommInterfaceImplementation implements MiddlewareCommInterface {
-    int rfid;
+  int rfid;
 
-    public TrackData requestRfid(int rfid){
-
-      this.rfid = rfid;
-      return new TrackData();
-    }
-  
-    public int getRfid(){
-        return rfid;
-    }
-  
+  public TrackData requestRfid(int rfid){
+    this.rfid = rfid;
+    return new TrackData();
+  }
+  public int getRfid(){
+    return rfid;
+  } 
 } 
--- src/test/java/edu/oakland/test/middleware02/RequestRfidTest.java
+++ src/test/java/edu/oakland/test/middleware02/RequestRfidTest.java
@@ -2,22 +2,20 @@ package edu.oakland.test.middleware02;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-
 import edu.oakland.production.middleware02.MiddlewareCommInterface;
 import edu.oakland.production.middleware02.MiddlewareCommInterfaceImplementation;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName(" Rfid Rquest Test")
 public class RequestRfidTest{
     
-    @Test
-    @DisplayName("Rfid In Is the same Rfid Out")
+  @Test
+  @DisplayName("Rfid In Is the same Rfid Out")
     void RfidInIsRfidOut(){
-        int RanRfid = (int)(Math.floor(100000 + Math.random() * 900000));
-        MiddlewareCommInterfaceImplementation Rfid = new MiddlewareCommInterfaceImplementation(RanRfid);
-        assertEquals(RanRfid, Rfid.getRfid());
+    int RanRfid = (int)(Math.floor(100000 + Math.random() * 900000));
+    MiddlewareCommInterfaceImplementation Rfid = new MiddlewareCommInterfaceImplementation();
+    assertEquals(RanRfid, Rfid.getRfid());
 
     }
 }
